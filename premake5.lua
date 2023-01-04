03761d77b48d82c2ba23cceebab92f1e2a72f153
Lucca Troll <lucca-troll@web.de>
2023-01-05 00:29:44 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,82 +1,119 @@
-project "Assimp"
+project "ImGui"
 	kind "StaticLib"
 	language "C++"
+	cppdialect "C++17"
 	staticruntime "off"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
-	files
+	defines
 	{
-		"include/GLFW/glfw3.h",
-		"include/GLFW/glfw3native.h",
-		"src/glfw_config.h",
-		"src/context.c",
-		"src/init.c",
-		"src/input.c",
-		"src/monitor.c",
-
-		"src/null_init.c",
-		"src/null_joystick.c",
-		"src/null_monitor.c",
-		"src/null_window.c",
-
-		"src/platform.c",
-		"src/vulkan.c",
-		"src/window.c",
+		-- "SWIG",
+		"ASSIMP_BUILD_NO_OWN_ZLIB",
+  
+		"ASSIMP_BUILD_NO_X_IMPORTER",
+		"ASSIMP_BUILD_NO_3DS_IMPORTER",
+		"ASSIMP_BUILD_NO_MD3_IMPORTER",
+		"ASSIMP_BUILD_NO_MDL_IMPORTER",
+		"ASSIMP_BUILD_NO_MD2_IMPORTER",
+		"ASSIMP_BUILD_NO_PLY_IMPORTER",
+		"ASSIMP_BUILD_NO_ASE_IMPORTER",
+		-- "ASSIMP_BUILD_NO_OBJ_IMPORTER",
+		"ASSIMP_BUILD_NO_AMF_IMPORTER",
+		"ASSIMP_BUILD_NO_HMP_IMPORTER",
+		"ASSIMP_BUILD_NO_SMD_IMPORTER",
+		"ASSIMP_BUILD_NO_MDC_IMPORTER",
+		"ASSIMP_BUILD_NO_MD5_IMPORTER",
+		"ASSIMP_BUILD_NO_STL_IMPORTER",
+		"ASSIMP_BUILD_NO_LWO_IMPORTER",
+		"ASSIMP_BUILD_NO_DXF_IMPORTER",
+		"ASSIMP_BUILD_NO_NFF_IMPORTER",
+		"ASSIMP_BUILD_NO_RAW_IMPORTER",
+		"ASSIMP_BUILD_NO_OFF_IMPORTER",
+		"ASSIMP_BUILD_NO_AC_IMPORTER",
+		"ASSIMP_BUILD_NO_BVH_IMPORTER",
+		"ASSIMP_BUILD_NO_IRRMESH_IMPORTER",
+		"ASSIMP_BUILD_NO_IRR_IMPORTER",
+		"ASSIMP_BUILD_NO_Q3D_IMPORTER",
+		"ASSIMP_BUILD_NO_B3D_IMPORTER",
+		-- "ASSIMP_BUILD_NO_COLLADA_IMPORTER",
+		"ASSIMP_BUILD_NO_TERRAGEN_IMPORTER",
+		"ASSIMP_BUILD_NO_CSM_IMPORTER",
+		"ASSIMP_BUILD_NO_3D_IMPORTER",
+		"ASSIMP_BUILD_NO_LWS_IMPORTER",
+		"ASSIMP_BUILD_NO_OGRE_IMPORTER",
+		"ASSIMP_BUILD_NO_OPENGEX_IMPORTER",
+		"ASSIMP_BUILD_NO_MS3D_IMPORTER",
+		"ASSIMP_BUILD_NO_COB_IMPORTER",
+		"ASSIMP_BUILD_NO_BLEND_IMPORTER",
+		"ASSIMP_BUILD_NO_Q3BSP_IMPORTER",
+		"ASSIMP_BUILD_NO_NDO_IMPORTER",
+		"ASSIMP_BUILD_NO_IFC_IMPORTER",
+		"ASSIMP_BUILD_NO_XGL_IMPORTER",
+		-- "ASSIMP_BUILD_NO_FBX_IMPORTER",
+		"ASSIMP_BUILD_NO_ASSBIN_IMPORTER",
+		-- "ASSIMP_BUILD_NO_GLTF_IMPORTER",
+		"ASSIMP_BUILD_NO_C4D_IMPORTER",
+		"ASSIMP_BUILD_NO_3MF_IMPORTER",
+		"ASSIMP_BUILD_NO_X3D_IMPORTER",
+		"ASSIMP_BUILD_NO_MMD_IMPORTER",
+		
+		"ASSIMP_BUILD_NO_STEP_EXPORTER",
+		"ASSIMP_BUILD_NO_SIB_IMPORTER",
+  
+		-- "ASSIMP_BUILD_NO_MAKELEFTHANDED_PROCESS",
+		-- "ASSIMP_BUILD_NO_FLIPUVS_PROCESS",
+		-- "ASSIMP_BUILD_NO_FLIPWINDINGORDER_PROCESS",
+		-- "ASSIMP_BUILD_NO_CALCTANGENTS_PROCESS",
+		"ASSIMP_BUILD_NO_JOINVERTICES_PROCESS",
+		-- "ASSIMP_BUILD_NO_TRIANGULATE_PROCESS",
+		"ASSIMP_BUILD_NO_GENFACENORMALS_PROCESS",
+		-- "ASSIMP_BUILD_NO_GENVERTEXNORMALS_PROCESS",
+		"ASSIMP_BUILD_NO_REMOVEVC_PROCESS",
+		"ASSIMP_BUILD_NO_SPLITLARGEMESHES_PROCESS",
+		"ASSIMP_BUILD_NO_PRETRANSFORMVERTICES_PROCESS",
+		"ASSIMP_BUILD_NO_LIMITBONEWEIGHTS_PROCESS",
+		-- "ASSIMP_BUILD_NO_VALIDATEDS_PROCESS",
+		"ASSIMP_BUILD_NO_IMPROVECACHELOCALITY_PROCESS",
+		"ASSIMP_BUILD_NO_FIXINFACINGNORMALS_PROCESS",
+		"ASSIMP_BUILD_NO_REMOVE_REDUNDANTMATERIALS_PROCESS",
+		"ASSIMP_BUILD_NO_FINDINVALIDDATA_PROCESS",
+		"ASSIMP_BUILD_NO_FINDDEGENERATES_PROCESS",
+		"ASSIMP_BUILD_NO_SORTBYPTYPE_PROCESS",
+		"ASSIMP_BUILD_NO_GENUVCOORDS_PROCESS",
+		"ASSIMP_BUILD_NO_TRANSFORMTEXCOORDS_PROCESS",
+		"ASSIMP_BUILD_NO_FINDINSTANCES_PROCESS",
+		"ASSIMP_BUILD_NO_OPTIMIZEMESHES_PROCESS",
+		"ASSIMP_BUILD_NO_OPTIMIZEGRAPH_PROCESS",
+		"ASSIMP_BUILD_NO_SPLITBYBONECOUNT_PROCESS",
+		"ASSIMP_BUILD_NO_DEBONE_PROCESS",
+		"ASSIMP_BUILD_NO_EMBEDTEXTURES_PROCESS",
+		"ASSIMP_BUILD_NO_GLOBALSCALE_PROCESS"
 	}
 
-	filter "system:linux"
-		pic "On"
+	files
+	{
+		"include/**",
+		"code/CApi/**",
+		"code/Common/**",
+		"code/Material/**",
+		"code/Pbrt/**",
+		"code/PostProcessing/**",
+		"code/res/**"
+		"contrib/irrXML/*"
+	}
 
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
+	includedirs
+	{
+		"include",
+		"contrib/irrXML",
+		"contrib/zlib",
+		"contrib/rapidjson/include"
+	}
 
 	filter "system:windows"
 		systemversion "latest"
-
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
-
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
-
-		links
-		{
-			"Dwmapi.lib"
-		}
 
 	filter "configurations:Debug"
 		runtime "Debug"
